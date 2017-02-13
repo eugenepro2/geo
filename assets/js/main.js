42b62261f3871a915f06d0fc6e2dd968ee54397d
Your Name <you@example.com>
2017-02-13 11:47:43 +0200

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,65 @@
 $(function() {
-	
+  $('.table').wrap('<div style="overflow-x: auto"></div>');
+  $(document).on('scroll', function() {$('.icon-burger').stop(true, false).animate({'top': $(window).height()/2 + $(window).scrollTop()}, 100)})
+
+  $(window).resize(function() {
+    slideout.close();
+  });
+
+
 });
+
+//Modal
+var modal = new tingle.modal();
+modal.setContent(document.querySelector('.call-back').innerHTML);
+var city = new tingle.modal();
+city.setContent(document.querySelector('.city').innerHTML);
+
+
+//SlideOut
+var slideout = new Slideout({
+  'panel': document.getElementById('panel'),
+  'menu': document.getElementById('menu'),
+  'padding': 256,
+  'tolerance': 70
+});
+document.querySelector('.toggle-menu').addEventListener('click', function() {
+  slideout.toggle();
+});
+
+//Gallery
+baguetteBox.run('.gallery');
+
+
+//Map
+ymaps.ready(init);
+var myMap;
+
+function init(){     
+ var myMap = new ymaps.Map('map', {
+      // При инициализации карты, обязательно нужно указать
+      // ее центр и коэффициент масштабирования
+      center: [55.7247, 37.6331],
+      zoom: 17
+  });
+ 
+  // Создание метки 
+  var myPlacemark = new ymaps.Placemark(
+  // Координаты метки
+  [55.7247,37.6331] , {
+              // Свойства
+              // Текст метки
+              hintContent: 'Россия, Москва, ул. Щипок, д. 16, строение 1, офис 24'
+          }, {
+              iconImageHref: 'img/marker.svg', // картинка иконки
+              iconImageSize: [50, 50], // размеры картинки
+              iconImageOffset: [-10, -40] // смещение картинки
+              });     
+
+
+  // Добавление метки на карту
+  myMap.geoObjects.add(myPlacemark);
+}
 
 
 //Ajax отправка формы
@@ -35,4 +94,15 @@ function sendAjaxForm(form, event) {
     return false;
   }
 }
-  
+
+//TextArea
+var textarea = document.querySelector('textarea');
+textarea.addEventListener('keydown', autosize);
+ 
+function autosize () {
+    var el = this;
+    setTimeout(function () {
+        el.style.cssText = 'height:auto;';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+    }, 0);
+}
